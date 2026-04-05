f724f0529c14b228c9d947816554db052f226c20
herba-wild <912605379@qq.com>
2026-04-05 19:29:58 +0800

    这个版本用于合并表格便于获得面板数据

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import numpy as np
 import pandas as pd
+import glob
 
-year=2024
-data=pd.read_excel('{0}.xls'.format(year))
-print(data)
-data=data.drop([0,1,3,4,5,6,7,8])
-print(data)
-data=data.drop('Unnamed: 1',axis=1)
-print(data)
-data.to_excel('{0}.xlsx'.format(year),index=False)
+files=glob.glob('*.xlsx')
+col=1
+print(files)
+
+dfs=[]
+
+for file in files:
+    year=file.replace('.xlsx','')
+
+    df=pd.read_excel(file,index_col=0)
+
+    df=df.iloc[:,[2]]
+    df.iloc[0]=year
+    print(df)
+    dfs.append(df)
+
+result=pd.concat(dfs,axis=1)
+print(result)
+result.to_excel('货 运 量.xlsx',index=True)
