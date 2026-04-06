0ca6001da4f3ca7aedf6e3410ef223de8e512c23
herba-wild <912605379@qq.com>
2026-04-06 18:56:29 +0800

    这个版本用于合并表格便于获得面板数据

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import glob
 
 files=glob.glob('*.xlsx')
 col=1
-print(files)
 
 dfs=[]
 
-column='化学纤维'
+column='水  泥'
 
 for file in files:
     year = file.replace('.xlsx', '')
 
     df = pd.read_excel(file, index_col=0,header=1)
+    print(df,year)
 
     df = df['{0}'.format(column)]
     df.name=year
@@ -21,5 +21,5 @@ for file in files:
     dfs.append(df)
 
 result = pd.concat(dfs, axis=1)
-print(result)
-result.to_excel('{0}.xlsx'.format(column), index=True)
+result=result.T
+result.to_excel('D:\PyCharm 2025.3.2.1\project\pyoffice\data\{0}.xlsx'.format(column), index=True)
