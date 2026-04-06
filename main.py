5b68143b1b998fec939861d4ff5dd23d748c25c3
herba-wild <912605379@qq.com>
2026-04-06 18:11:41 +0800

    这个版本用于合并表格便于获得面板数据

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ print(files)
 
 dfs=[]
 
+column='化学纤维'
+
 for file in files:
-    year=file.replace('.xlsx','')
+    year = file.replace('.xlsx', '')
 
-    df=pd.read_excel(file,index_col=0)
+    df = pd.read_excel(file, index_col=0,header=1)
 
-    df=df.iloc[:,[2]]
-    df.iloc[0]=year
+    df = df['{0}'.format(column)]
+    df.name=year
     print(df)
     dfs.append(df)
 
-result=pd.concat(dfs,axis=1)
+result = pd.concat(dfs, axis=1)
 print(result)
-result.to_excel('货 运 量.xlsx',index=True)
+result.to_excel('{0}.xlsx'.format(column), index=True)
